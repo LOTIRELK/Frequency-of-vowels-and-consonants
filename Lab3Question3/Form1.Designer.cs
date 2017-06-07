9789354cd08ba7e59d3c226ff96513b04e32f0cf
LOTIRELK <lorcan.james.otoole@gmail.com>
2017-06-07 12:59:06 +0100

    g
    g

---
+++ Lab3Question3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Lab3Question3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.txtVowel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOther = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(160, 190);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(123, 42);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "C&alculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(775, 190);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(123, 42);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "String:";
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(160, 29);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(738, 26);
+            this.txtString.TabIndex = 3;
+            // 
+            // txtVowel
+            // 
+            this.txtVowel.Location = new System.Drawing.Point(160, 88);
+            this.txtVowel.Name = "txtVowel";
+            this.txtVowel.Size = new System.Drawing.Size(100, 26);
+            this.txtVowel.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Vowels:";
+            // 
+            // txtOther
+            // 
+            this.txtOther.Location = new System.Drawing.Point(160, 135);
+            this.txtOther.Name = "txtOther";
+            this.txtOther.Size = new System.Drawing.Size(100, 26);
+            this.txtOther.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Other:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 244);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOther);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtVowel);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.TextBox txtVowel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOther;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Lab3Question3/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//a program that counts vowels in a string inutted by user and counts number of non-vowels
+namespace Lab3Question3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //convert string to uppercase(easier to find vowels)
+            string input = txtString.Text.ToUpper();
+            int vowelCount = 0;
+            int otherCount = 0;
+            //scan each char in string 
+            
+            foreach (char a in input)
+            {
+                //call function to check if the text box is empty
+                validate();
+                //call function that passes in char and returns true if char is vowel
+                //or false if non-vowel
+                bool isVowel = isVowelPresent(a);
+                if (isVowel == true)
+                {
+                    //if returns true incremnt vowelCounter by 1
+                    vowelCount++;
+                }
+                else
+                    //otherwise increment otherCount by 1
+                    otherCount++;
+            }
+            txtVowel.Text = vowelCount.ToString();
+            txtOther.Text = otherCount.ToString();
+        }
+
+        //declaration of function to return true if vowel is present or false if not
+        bool isVowelPresent(char v)
+        {
+            if (v == 'A' || v == 'E' || v == 'I' || v == 'O' || v == 'U')
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close form when exit button clicked
+            this.Close();
+        }
+        //declaration of function to check if emtpy
+        void validate()
+        {
+            if (txtString.Text == "")
+            {
+                MessageBox.Show(
+                    "Please enter a sentence. Space must not be empty", "Error!");
+                this.Close();
+            }
+        }
+    }
+}
